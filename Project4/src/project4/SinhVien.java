83536a5187110ff568c04071758ec2a742d52a17
luongcongduy <nikdrago123@gmail.com>
2018-04-23 03:36:43 +0700

    TH4

---
+++ Project4/src/project4/SinhVien.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project4;
+
+/**
+ *
+ * @author theunforgiven
+ */
+public class SinhVien {
+    String MSSV,Ten,Nam,Lop;
+    public void setMSSV(String mssv)
+    {
+        MSSV=mssv;
+    }
+    public String getMSSV()
+    {
+      return MSSV;
+    }
+    public void setTen(String ten)
+    {
+        Ten = ten ;
+    }
+    public String getTen()
+    {
+      return Ten;
+    }
+    
+    public void setNam (String nam)
+    {
+        Nam=nam;
+    }
+    public String getNam()
+    {
+        return Nam;
+    }
+    
+    public void setLop(String lop)
+    {
+        Lop = lop;
+    }
+    
+    public String getLop()
+    {
+        return Lop;
+    }
+}
+
+
